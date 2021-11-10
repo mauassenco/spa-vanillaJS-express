da79264ec67c784c6c43a6f951618e222d9102ba
Mauricio Assenço <mauricio.assenco@juni.com.br>
2021-11-10 15:59:51 -0300

    changing frontend folder name to docs

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require ("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve("frontend", "static")));
+app.use("/static", express.static(path.resolve(__dirname, "docs", "static")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "docs", "index.html"));
 });
 
 app.listen(process.env.PORT || 9000, () => console.log("Servidor funcionando"));
